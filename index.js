1e362d9ef02904e8821ce422201676d332fd3b92
Andrey Solin <solinandrey@gmail.com>
2023-06-15 21:54:42 +0400

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const API_TOKEN = '6215862830:AAFhUMn8HH4-EKw6vU0D604Y7pg_8rP1stQ'
+
+const bot = require("telegraf");
+
+const bot = new bot({
+  token: API_TOKEN,
+});
+
+bot.on("message", async (chat_id, text) => {
+  if (text === "/start") {
+    bot.sendMessage(chat_id, "Hello, world!");
+  }
+});
+
+bot.start();
